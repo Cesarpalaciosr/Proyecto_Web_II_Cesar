7c0da3e5047ccc7c2bca9b2c3e404008d165c4d6
Cesar <capr99@hotmail.com>
2021-03-11 19:27:22 -0400

    puerto de base de datos omitido xd

--- src/cp/helpers/ConnectionDB.java
+++ src/cp/helpers/ConnectionDB.java
@@ -16,7 +16,7 @@ Connection conn = null;
 		String user = "postgres";
 		String password = "masterkey";
 	*/	
-		String url = "jdbc:postgresql://"+"ec2-3-222-11-129.compute-1.amazonaws.com"+"/de511gfgus123n?sslmode=require";
+		String url = "jdbc:postgresql://"+"ec2-3-222-11-129.compute-1.amazonaws.com:5432"+"/de511gfgus123n?sslmode=require";
 		String user = "tryqteclkftanp";
 		String password = "05db7b52ac66289538be68d886f33753a8839d2de5c81f0272ec20f8760a8fd8" ;
 		
